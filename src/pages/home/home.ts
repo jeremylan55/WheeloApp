558d5d8e942d87c39bc48c3c0c12aacfe6c1c5ee
Jeremy Lan <jeremylan55@gmail.com>
2017-02-05 07:56:57 -0500

    Added page native transition to slide
    1. added Native-Page-Transitions plugin.
    2. Set flip transition when log in
    3. smooth slide skip

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import {NativePageTransitions, NativeTransitionOptions} from 'ionic-native';
 /*
   Generated class for the Home page.
 
@@ -16,6 +16,21 @@ export class HomePage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
+    let options: NativeTransitionOptions = {
+      "direction"      : "left",
+      "duration"       :  400, // in milliseconds (ms), default 400
+      "iosdelay"       :   60, // ms to wait for the iOS webview to update before animation kicks in, default 60
+      "androiddelay"   :  100
+    };
+
+    NativePageTransitions.flip(options)
+      .then((data) => {
+
+      })
+      .catch((err) => {
+
+      });
+
     console.log('ionViewDidLoad HomePage');
   }
 
--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,Slides } from 'ionic-angular';
 import { TabsPage } from '../tabs/tabs';
+import { ViewChild } from '@angular/core';
+import jQuery from "jquery";
 
 @Component({
   selector: 'page-intro',
   templateUrl: 'intro.html'
 })
 export class IntroPage {
-
+  @ViewChild(Slides) slides: Slides;
   sliderOptions: any;
 
   constructor(public navCtrl: NavController) {
-
+    this.navCtrl = navCtrl;
     this.sliderOptions = {
       pager: true
     };
@@ -27,10 +29,19 @@ export class IntroPage {
   }
 
   skip() {
-    //this.slides.slideTo(2);
+    this.slides.slideTo(2);
   }
-  notFistSlide() {
-    //jquery('#skip').hide();
+  hideSkip() {
+    // shows skip button only on first slide.
+    console.log('Slide changed');
+    let isFirst = this.slides.isBeginning();
+    if(isFirst) {
+      jQuery('#skip').fadeIn(150);
+    }
+    else {
+      jQuery('#skip').fadeOut(150);
+    }
+
   }
 
 }
