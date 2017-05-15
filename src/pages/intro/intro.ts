e81df80f7659b88086ef51dbc0f3b17327e6e265
Jeremy Lan <jeremylan55@gmail.com>
2017-05-14 23:33:57 -0400

    set login back

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -28,46 +28,46 @@ export class IntroPage {
 // Skips the facebook oath and jumps to Home page
 // Use this login for DEVELOPMENT ONLY!
 // Call this from login()
-login(){
-  let nav = this.navCtrl;
-  nav.setRoot(TabsPage);
-}
+// login(){
+//   let nav = this.navCtrl;
+//   nav.setRoot(TabsPage);
+// }
 
 // Logins to facebook and receive the toekn.
 // Use this when deploying.
 // Call this from login()
-	// login(){
-	// 	Facebook.browserInit(this.FB_APP_ID, "v2.8");
-	// 	let permissions = new Array();
-	// 	let nav = this.navCtrl;
-	// 	permissions = ['public_profile'];
-  //
-	// 	Facebook.login(permissions)
-	// 	.then(function(response){
-	// 		let userId = response.authResponse.userID;
-	// 		let params = new Array();
-	// 		let actoken = response.authResponse.accessToken;
-	// 		// Get name
-	// 		Facebook.api('/me?fields=name', params)
-	// 		.then(function(user){
-	// 			user.picture = 'https://graph.facebook.com/' + userId + '/picture?type=large';
-	// 			NativeStorage.setItem('user',
-	// 			{
-	// 				name: user.name,
-	// 				picture: user.picture,
-	// 				userID: userId,
-	// 				accessToken: actoken
-	// 			})
-	// 			.then(function(){
-	// 				nav.setRoot(TabsPage);
-	// 			}, function(error){
-	// 				console.log(error)
-	// 			})
-	// 		})
-	// 	}, function(error){
-	// 		console.log(error)
-	// 	});
-  // }
+	login(){
+		Facebook.browserInit(this.FB_APP_ID, "v2.8");
+		let permissions = new Array();
+		let nav = this.navCtrl;
+		permissions = ['public_profile'];
+
+		Facebook.login(permissions)
+		.then(function(response){
+			let userId = response.authResponse.userID;
+			let params = new Array();
+			let actoken = response.authResponse.accessToken;
+			// Get name
+			Facebook.api('/me?fields=name', params)
+			.then(function(user){
+				user.picture = 'https://graph.facebook.com/' + userId + '/picture?type=large';
+				NativeStorage.setItem('user',
+				{
+					name: user.name,
+					picture: user.picture,
+					userID: userId,
+					accessToken: actoken
+				})
+				.then(function(){
+					nav.setRoot(TabsPage);
+				}, function(error){
+					console.log(error)
+				})
+			})
+		}, function(error){
+			console.log(error)
+		});
+  }
 
 
   skip() {
